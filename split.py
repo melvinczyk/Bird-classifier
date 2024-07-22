0f3f2da807a89723b1ae5faddfd0e738c9e034d4
melvinczyk <nburczyk@icloud.com>
2024-07-22 17:56:11 -0500

    splitting

--- split.py
+++ split.py
@@ -1,29 +1,19 @@
 import os
 import pandas as pd
 
+
 train = 0.8
 val = 0.1
 test = 0.1
-kfolds = 1
-
-mels_path = './mels/'
-split_path = './mels/split/'
 
 birds = []
-bird_data = []
-all_files = []
-
-for root ,dirs, files in os.walk(mels_path):
-    for dir in dirs:
-        birds.append(dir)
-
-print(birds)
-
-train_set = []
-test_set = []
-val_set = []
+all_wav_files = []
 
 df = pd.read_csv('audio_files.csv')
-for nr, bird in enumerate(birds):
-    for root, dirs, files in os.walk(mels_path + bird):
-        continue
+
+for index, row in df.iterrows():
+    birds.append(row['Bird Name'])
+    all_wav_files.append([row['Bird Name'], row["File"]])
+birds = list(set(birds))
+print(birds)
+print(all_wav_files)
